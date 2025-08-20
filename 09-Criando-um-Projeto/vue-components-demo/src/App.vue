202ed0b6abaf82c049456a9ff1f3af421371082e
Davi <davi.rferreira@gmail.com>
2025-08-20 13:20:53 -0300

    Mudança na organização do repositório e trabalhando com um projeto com módulos do Vue instalados

---
+++ 09-Criando-um-Projeto/vue-components-demo/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    {{ message }}
+  </div>
+  <contact></contact>
+  <buttonCounter></buttonCounter>
+  <buttonCounter></buttonCounter>
+</template>
+
+<script setup>
+  import Contact from './components/Contact.vue';
+  const message = "Hello Vue"
+</script>
+
+<style></style>
---
+++ 09-Criando-um-Projeto/vue-components-demo/src/components/Contact.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Contact us</h1>
+    <p>Please send an e-mail to {{ email }}</p>
+  </div>
+</template>
+<script setup>
+  import { ref } from 'vue'
+  const email = ref("davi@email.com")
+</script>
