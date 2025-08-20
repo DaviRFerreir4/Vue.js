73f82844e3491d7dec0c197af0d9a804bf1c1fe9
Davi <davi.rferreira@gmail.com>
2025-08-20 14:39:54 -0300

    Chamando componentes localmente e entendendo props

--- 09-Criando-um-Projeto/vue-components-demo/src/App.vue
+++ 09-Criando-um-Projeto/vue-components-demo/src/App.vue
@@ -1,15 +1,68 @@
 <template>
-  <div>
-    {{ message }}
+  <div class="container">
+    <div class="bg-info rounded p-4 m-2">
+      <div class="row">
+        <label for="contact">Contact Owner Name</label>
+        <input
+          type="text"
+          name="contact"
+          id="contact"
+          class="mb-2"
+          v-model="contactName"
+        />
+        <div
+          class="col-12 bg-secondary mb-2 rounded"
+          v-for="contact in contacts"
+          :key="contact.name"
+        >
+          <input
+            type="text"
+            name="contactName"
+            id="contactName"
+            class="mb-2"
+            v-model.lazy="contact.name"
+          />
+          <Contact
+            :name="contact.name"
+            :phone="contact.phone"
+            :ownername="contact.ownername"
+            :is_favorite="contact.is_favorite"
+          ></Contact>
+        </div>
+      </div>
+    </div>
   </div>
-  <contact></contact>
-  <buttonCounter></buttonCounter>
-  <buttonCounter></buttonCounter>
 </template>
 
 <script setup>
-  import Contact from './components/Contact.vue';
-  const message = "Hello Vue"
+// import ButtonCounter from "./components/ButtonCounter.vue"
+// const message = "Hello Vue"
+import { reactive, ref } from "vue"
+import Contact from "./components/Contact.vue"
+const contactName = ref("github.com/DaviRFerreir4")
+
+const contacts = reactive([
+  {
+    name: "Davi",
+    phone: 40028922,
+    // passando um valor dinâmico como prop
+    ownername: contactName,
+    is_favorite: false,
+  },
+  {
+    name: "Roberto",
+    phone: 45211896,
+    // passando um valor fixo como prop
+    ownername: contactName.value,
+    is_favorite: true,
+  },
+  {
+    name: "Alexandre",
+    phone: 95423687,
+    ownername: contactName,
+    is_favorite: false,
+  },
+])
 </script>
 
 <style></style>
--- 09-Criando-um-Projeto/vue-components-demo/src/components/Contact.vue
+++ 09-Criando-um-Projeto/vue-components-demo/src/components/Contact.vue
@@ -1,10 +1,19 @@
 <template>
-  <div>
-    <h1>Contact us</h1>
-    <p>Please send an e-mail to {{ email }}</p>
-  </div>
+  <h3>Name: {{ name }}</h3>
+  <p>Email: {{ email }}</p>
+  <p>Phone: {{ phone }}</p>
+  <p class="float-end small" v-if="ownername !== ''">
+    * this contact info belongs to {{ ownername }}
+  </p>
 </template>
 <script setup>
-  import { ref } from 'vue'
-  const email = ref("davi@email.com")
+import { ref } from "vue"
+// const email = ref("davi@email.com") // tem preferência acima da prop no chamado do componente
+const props = defineProps({
+  name: { type: String, required: true },
+  phone: Number,
+  ownername: String,
+  email: { type: String, required: false, default: "- n/a -" },
+  is_favorite: Boolean,
+})
 </script>
