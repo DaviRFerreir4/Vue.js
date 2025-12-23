e1326e3d2ebee71932447604ee6906e7a8744e65
Davi <davi.rferreira@gmail.com>
2025-12-23 10:04:30 -0300

    Trabalhando com emits

--- Rocketseat/01-Teoria/01-Fundamentos-do-Vue/src/App.vue
+++ Rocketseat/01-Teoria/01-Fundamentos-do-Vue/src/App.vue
@@ -1,28 +1,55 @@
 <template>
-  <div class="container-box">
+  <div class="container">
     <Box name="Davi" :age="22" />
     <Box name="Thiago" :age="18" />
     <Box name="Murilo" :age="34" status="inactive" />
   </div>
+  <div class="container">
+    <div class="button-container">
+      <h2>{{ count }}</h2>
+      <Button @increment="incrementCount" />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-  import Box from './components/box.vue'
+  import Box from './components/Box.vue'
+  import Button from './components/Button.vue'
 
   export default {
     name: 'App',
     components: {
       Box,
+      Button,
+    },
+    data() {
+      return {
+        count: 0,
+      }
+    },
+    methods: {
+      incrementCount() {
+        this.count++
+      },
     },
   }
 </script>
 
 <style scoped>
-  .container-box {
+  .container {
     padding: 10px;
     border: 1px solid black;
     display: flex;
     justify-content: center;
     gap: 10px;
+
+    & + & {
+      margin-top: 20px;
+    }
+  }
+
+  .button-container {
+    display: grid;
+    justify-items: center;
   }
 </style>
---
+++ Rocketseat/01-Teoria/01-Fundamentos-do-Vue/src/components/Button.vue
@@ -0,0 +1,25 @@
+<template>
+  <button @click="$emit('increment')">+1</button>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'Button',
+  }
+</script>
+
+<style scoped>
+  button {
+    padding: 10px 20px;
+    border: none;
+    border-radius: 8px;
+
+    color: white;
+    background-color: #42b983;
+    cursor: pointer;
+
+    &:hover {
+      background-color: #61e819;
+    }
+  }
+</style>
