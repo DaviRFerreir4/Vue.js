8c60ce2ef84a1707925cbb353470360b7544ce2a
Davi <davi.rferreira@gmail.com>
2025-12-23 09:27:59 -0300

    Iniciando curso da Rocketseat com a options API

---
+++ Rocketseat/01-Teoria/01-Fundamentos-do-Vue/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container-box">
+    <Box />
+    <Box />
+    <Box />
+  </div>
+</template>
+
+<script lang="ts">
+  import Box from './components/box.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      Box,
+    },
+  }
+</script>
+
+<style scoped>
+  .container-box {
+    padding: 10px;
+    border: 1px solid black;
+    display: flex;
+    justify-content: center;
+    gap: 10px;
+  }
+</style>
---
+++ Rocketseat/01-Teoria/01-Fundamentos-do-Vue/src/components/Box.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="box">1</div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'Box',
+  }
+</script>
+
+<style scoped>
+  .box {
+    width: 30px;
+    height: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    background-color: red;
+  }
+</style>
